cff4726f3febbc92d28baa8e7a2dc7e64325597e
Bishal Sarkar <69084883+bishalsarkar77730@users.noreply.github.com>
2020-08-01 22:15:39 +0530

    Add files via upload

---
+++ project dictionary minor.py
@@ -0,0 +1,9 @@
+print("welcome to your own dictionary")
+Dictionary={"hello":"used as a greeting or to begin a telephone conversation",
+            "apple":"the round fruit of a tree of the rose family",
+            "necessary":"needed to be done, achieved, or present; essential.",
+            "essential":"absolutely necessary; extremely important."}
+print("search the word ")
+search=input()
+print(Dictionary[search])
+print("thanks for using dictionary")
